73e713887e227d7086f799c63f7cbe05a1a06da0
soso7194 <wons5215@naver.com>
2025-07-31 22:47:19 +0900

    refactor TimeAttack

--- Assets/01Script/SDW/TimeAttack.cs
+++ Assets/01Script/SDW/TimeAttack.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class TimeAttack : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float timeLimit = 60f; // Time limit in seconds
+    private float timeRemaining;
+    private bool isTimeUp = false;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        timeRemaining = timeLimit;
+    }
+    private void Update()
+    {
+        if (!isTimeUp)
+        {
+            timeRemaining -= Time.deltaTime;
+            if (timeRemaining <= 0)
+            {
+                timeRemaining = 0;
+                isTimeUp = true;
+                OnTimeUp();
+            }
+        }
+    }
+    private void OnTimeUp()
+    {
+        // Handle what happens when time is up
+        Debug.Log("Time is up!");
+        // You can add more logic here, like ending the game or showing a message
     }
 }
